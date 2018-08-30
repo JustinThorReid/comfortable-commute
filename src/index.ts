7fb3be4479f3e363e2f9b88143428843b79c3859
Justin Reid <phatness0@gmail.com>
2018-08-29 23:56:18 -0400

    Move forcast to a function make stub html pages

--- src/index.ts
+++ src/index.ts
@@ -5,49 +5,64 @@ import { DataRecord } from './tables'
 
 const app = express()
 
-app.use('/', (req, res) => {
-	res.send("hi")
+function forecast(time: number = undefined): Promise<DataRecord> {
+	let forecastUrl = `${process.env.darksky_host}/forecast/${process.env.darksky_secret}/${process.env.work_lat},${process.env.work_lng}`
+	if (time) {
+		forecastUrl += `,${time}`
+	}
+	forecastUrl += `?exclude=minutely,hourly,daily,alerts,flags`
+
+	return new Promise<DataRecord>((resolve, reject) => {
+		request({
+			url: forecastUrl,
+			method: 'GET',
+			json: true
+		}, (error, _response, body) => {
+			if (error || !body || !body.currently) {
+				reject()
+				return
+			}
+
+			const currently = body.currently
+
+			const newRecord: DataRecord = {
+				time: currently.time,
+				precipType: currently.summary,
+				latitude: parseFloat(process.env.work_lat),
+				longitude: parseFloat(process.env.work_lng),
+				dewPoint: currently.dewPoint,
+				humidity: currently.humidity,
+				pressure: currently.pressure,
+				windSpeed: currently.windSpeed,
+				windGust: currently.windGust,
+				windBearing: currently.windBearing,
+				cloudCover: currently.cloudCover,
+				uvIndex: currently.uvIndex,
+				visibility: currently.visibility,
+				ozone: currently.ozone,
+				precipProbability: currently.precipProbability,
+				precipIntensity: currently.precipIntensity,
+				precipIntensityError: currently.precipIntensityError || 0,
+				temperature: currently.temperature,
+				feelsTemperature: currently.apparentTemperature
+			}
+
+			resolve(newRecord)
+		})
+	})
+}
+
+app.get('/api/forecast/now', async (_req, res) => {
+	try {
+		const result = await forecast()
+		res.send(result)
+	} catch (err) {
+		console.error("Forcast error", err)
+	}
 })
 
-app.listen(8080, () => {
-	console.log("listen")
-})
+app.use('/', express.static('static'))
 
-const CURRENT_URL = `${process.env.darksky_host}/forecast/${process.env.darksky_secret}/${process.env.work_lat},${process.env.work_lng}?exclude=minutely,hourly,daily,alerts,flags`
-console.log(CURRENT_URL)
-
-request({
-	url: CURRENT_URL,
-	method: 'GET',
-	json: true
-}, (error, response, body) => {
-	if (!body || !body.currently) {
-		return
-	}
-
-	const currently = body.currently
-
-	const newRecord: DataRecord = {
-		time: currently.time,
-		precipType: currently.summary,
-		latitude: parseFloat(process.env.work_lat),
-		longitude: parseFloat(process.env.work_lng),
-		dewPoint: currently.dewPoint,
-		humidity: currently.humidity,
-		pressure: currently.pressure,
-		windSpeed: currently.windSpeed,
-		windGust: currently.windGust,
-		windBearing: currently.windBearing,
-		cloudCover: currently.cloudCover,
-		uvIndex: currently.uvIndex,
-		visibility: currently.visibility,
-		ozone: currently.ozone,
-		precipProbability: currently.precipProbability,
-		precipIntensity: currently.precipIntensity,
-		precipIntensityError: currently.precipIntensityError || 0,
-		temperature: currently.temperature,
-		feelsTemperature: currently.apparentTemperature
-	}
-
-	console.log(newRecord)
+app.listen(process.env.PORT, () => {
+	console.log(`listening on ${process.env.PORT}`)
 })
